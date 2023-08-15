1eb5174ef305394b76abc08996f893eea6dc187f
ProWebbb0 <prowebbb0@gmail.com>
2023-08-15 12:40:51 +0500

    added product model

--- core/app/admin.py
+++ core/app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, ProductOption, Product, ProductImage
 
 
 @admin.register(Category)
@@ -8,3 +8,18 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ("pk", "name")
     list_display_links = ("pk", "name")
     prepopulated_fields = {"slug": ("name",)}
+
+
+class ProductOptionAdmin(admin.TabularInline):
+    model = ProductOption
+    extra = 1
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductOptionAdmin, ProductImageInline]
--- core/app/models.py
+++ core/app/models.py
@@ -11,3 +11,63 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    SIZE_CHOICES = [
+        (f'{i}', f'{i} мм')
+        for i in range(1, 30)
+    ]
+
+    QUANTITY_CHOICES = [
+        (i, i)
+        for i in range(1, 30)
+    ]
+    CORNICE_TYPE_CHOICES = [
+        ('Алюминевый', 'Алюминевый'),
+        ('Пластиковый', 'Пластиковый')
+    ]
+
+    CONTROL_TYPE_CHOICES = [
+        ('', 'Ручной'),
+        ('С электроприводом', 'С электроприводом'),
+    ]
+
+    name = models.CharField(verbose_name="Название товара", unique=True, max_length=155)
+    width = models.CharField(verbose_name="Ширина", max_length=15, choices=SIZE_CHOICES)
+    height = models.CharField(verbose_name="Высота", max_length=15, choices=SIZE_CHOICES)
+    is_left_control = models.BooleanField(verbose_name="Управление слева?", default=False, blank=True)
+    is_right_control = models.BooleanField(verbose_name="Управление Справа?", default=False, blank=True)
+    quantity = models.IntegerField(verbose_name="Количество", choices=QUANTITY_CHOICES)
+    cornice_type = models.CharField(blank=True, choices=CORNICE_TYPE_CHOICES, max_length=50)
+    control_type = models.CharField(blank=True, choices=CONTROL_TYPE_CHOICES, max_length=50)
+    manufacturer_country = models.CharField(verbose_name="Страна производитель", max_length=150)
+    fabric_type = models.CharField(verbose_name="Тип ткани", max_length=150)
+    property = models.CharField(verbose_name="Свойство", max_length=100)
+    dimming = models.SmallIntegerField(verbose_name="Затемнение", default=0)
+    price = models.CharField(verbose_name="Цена", max_length=100)
+    description = models.TextField(verbose_name="Описание продукта")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class ProductOption(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_options")
+    option_main = models.CharField(verbose_name="Главная характеристика", max_length=200)
+    option_additional = models.TextField(verbose_name="Описание характеристики")
+
+    def __str__(self):
+        return f"{self.product}: {self.option_main}"
+
+
+class ProductImage(models.Model):
+    def make_folder_path(self, filename):
+        return f"photos/products/{filename}"
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    photo = models.ImageField(verbose_name="Фото", upload_to=make_folder_path)
