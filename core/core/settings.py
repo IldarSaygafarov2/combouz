8691617cb8c29da4eccbc1753c4a27f7f4ec767c
Saygafarov Ildar <sayildar17@gmail.com>
2023-09-17 03:39:55 +0500

    added fixes

--- core/core/settings.py
+++ core/core/settings.py
@@ -5,9 +5,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-pt495=&=psj+i&wua*4mh026y5n7y4%72=$tj+lhh(sm@osl_v"
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "combouz.pythonanywhere.com"
+]
 
 INSTALLED_APPS = [
     'jazzmin',
